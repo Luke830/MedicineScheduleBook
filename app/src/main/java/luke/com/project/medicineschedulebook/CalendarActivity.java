dc23f41a7c17ed2b25676d6f89e3875006888024
freesoul1983@gmail.ocm <water830>
2017-03-16 23:49:08 +0900

    수정 2

--- app/src/main/java/luke/com/project/medicineschedulebook/CalendarActivity.java
+++ app/src/main/java/luke/com/project/medicineschedulebook/CalendarActivity.java
@@ -3,6 +3,7 @@ package luke.com.project.medicineschedulebook;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -19,7 +20,7 @@ import java.util.HashMap;
  */
 
 
-public class CalendarActivity extends AppCompatActivity {
+public class CalendarActivity extends FragmentActivity {
 
     ViewPager pager;
     PagerAdapter pagerAdapter;
